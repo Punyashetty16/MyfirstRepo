5f477ab0cce61dfbae3fe84f75e396e4f5e7449d
Punyashetty16 <shettypunya06@gmail.com>
2024-11-18 13:37:43 +0530

    Add files via upload

---
+++ FileDemo.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+public class FileDemo
+{
+  public static void main(String args[])
+  {
+    Scanner br = new Scanner(System.in);
+    DateFormat sdf = new SimpleDateFormat("MMMM dd, YYY hh:mm a");
+    System.out.println("Enter the file name : ");
+    String Filename=br.nextLine();
+    File f = new File(Filename);
+    String extension = null;
+    if(f.isDirectory())
+    {
+      System.out.println(f.getName() +"is a directory");
+      System.out.println("Directory size "+f.length()+"bytes");
+      System.out.println("File last modified "+sdf.format(f.lastModified()));
+       System.out.println("List of files:");
+       String dir[]=f.list();
+       for(int i=0;i<dir.length;i++)
+       {
+          System.out.println(dir[i]);
+       }
+    }
+   if(f.isFile())
+    {
+      int i=Filename.lastIndexOf(".");
+      if(i>0)
+      {
+        extension=Filename.substring(i+1);
+      }
+       System.out.println("File Type : "+extension);
+        System.out.println(f.getName()+"is a file");
+        if(f.canRead())
+        {
+         System.out.println(Filename+"is readable");
+        }
+        else
+        {
+           System.out.println(Filename+"is not readable");
+        }
+        if(f.canWrite())
+        System.out.println(Filename+"is writable");
+        else
+        {
+         System.out.println(Filename+"is not writable");
+        }
+         System.out.println("File size is "+f.length()+"bytes");
+         System.out.println("Last modified : "+sdf.format(f.lastModified()));
+      }
+         else
+          {
+             System.out.println(f.getName()+" File does not exist ");
+           }
+      }
+  }
+  
+         
